b3bf005040e535e479e6f4389c81b1e0f620017c
Douglas Eleuterio <elettelecom@gmail.com>
2019-09-28 12:13:01 -0300

    Primeiro Commit
    Configuração do projeto

---
+++ app.ts
@@ -0,0 +1,12 @@
+let message : String = "Help me, Obi-Wan Kenobi, You're my only hope!";
+console.log(message);
+
+let episode: number = 4
+
+console.log("This is episode" + 4)
+episode = episode + 1
+console.log("Next episode is" + episode)
+
+let myFavoriteDroid;
+myFavoriteDroid = 'BB-8'
+console.log("My Favorite Droid is: " +myFavoriteDroid);
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi, You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log("This is episode" + 4);
+episode = episode + 1;
+console.log("Next episode is" + episode);
+var myFavoriteDroid;
+myFavoriteDroid = 'BB-8';
+console.log("My Favorite Droid is: " + myFavoriteDroid);
